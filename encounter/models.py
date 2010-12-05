77224ecd7701c7f7d25e38571338f5bcddc3532e
Sean Nicolay <nicolay2@illinois.edu>
2010-12-05 16:18:04 -0600

    Implemented views and templates for current models
    Created [modelname]_table.html templates to render basic table using a list of elements in that model. Also added named urls in urls.py, that way the urls template tag will work.

--- encounter/models.py
+++ encounter/models.py
@@ -18,6 +18,29 @@ class CreatureClass(models.Model):
     maximum_ref = models.IntegerField(blank=True)
     minimum_will = models.IntegerField(blank=True)
     maximum_will = models.IntegerField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _range(min, max):
+        if (min == max):
+            return str(max)
+        else:
+            return '[%d,%d]' % (min, max)
+
+    def hp(self):
+        return _range(self.minimum_hp, self.maximum_hp)
+
+    def ac(self):
+        return _range(self.minimum_ac, self.maximum_ac)
+
+    def fort(self):
+        return _range(self.minimum_fort, self.maximum_fort)
+
+    def ref(self):
+        return _range(self.minimum_ref, self.maximum_ref)
+
+    def will(self):
+        return _range(self.minimum_will, self.maximum_will)
 
     def new_instance(self, encounter):
         return encounter.new_creature(self.pk)
@@ -25,11 +48,21 @@ class CreatureClass(models.Model):
 class CreatureInstance(models.Model):
     creature_class = models.ForeignKey(CreatureClass, related_name='instances')
     encounter = models.ForeignKey(Encounter, related_name='creatures')
-    instance_id = models.IntegerField()
+    encounter_label = models.CharField(max_length=50, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def new_action(self, action):
         return self.actions.create(action)
+
+    def clean_encounter_label(self):
+        label = self.cleaned_data['encounter_label']
+        try:
+            if (self.encounter.creatures.get(encounter_label=label).pk != self.pk):
+                return label
+            else:
+                raise forms.ValidationError('label already exists in encounter %d' % self.encounter.pk)
+        except ObjectDoesNotExist:
+            return label
 
 class Action(models.Model):
     target = models.ForeignKey(CreatureInstance, related_name='actions')
--- urls.py
+++ urls.py
@@ -1,11 +1,20 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.generic import list_detail
+from encounter.models import Encounter, CreatureClass
 
 admin.autodiscover()
+
+encounters = { 'queryset' : Encounter.objects.all() }
+creature_classes = { 'queryset' : CreatureClass.objects.all() }
 
 urlpatterns = patterns('',
     # Example:
     # (r'^dm_nightmare/', include('dm_nightmare.foo.urls')),
+    (r'^encounters/creatures/', list_detail.object_list, creature_classes, 'creature_class_list'),
+    (r'^encounters/(?P<object_id>\d+)/', list_detail.object_detail,
+     encounters, 'encounter_detail'),
+    (r'^encounters/', list_detail.object_list, encounters, 'encounter_list'),
 
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
